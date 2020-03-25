0b3fc9343e35f4d54f9a68ed72318f108d16359d
heyulin <heyulin@sensetime.com>
2020-03-25 17:47:14 +0800

    code pep8 format

--- cpu-gpu-mem-monitor.py
+++ cpu-gpu-mem-monitor.py
@@ -1,14 +1,13 @@
+import ast
+import pandas as pd
+import datetime
+import time
+import pynvml
+import psutil
+import numpy as np
+import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import numpy as np
-import psutil
-import pynvml
-import time
-import datetime
-import pandas as pd
-import ast
-import numpy as np
 
 
 class CpuGpuMemoryInfo(object):
@@ -19,7 +18,7 @@ class CpuGpuMemoryInfo(object):
 
         # 采样间隔, 默认设置为5秒
         self.interval = 5
-        
+
         # data dict
         self.data_dict = {}
 
@@ -39,17 +38,17 @@ class CpuGpuMemoryInfo(object):
         _handle = pynvml.nvmlDeviceGetHandleByIndex(0)
         _info = pynvml.nvmlDeviceGetMemoryInfo(_handle)
         self.total_gpu_mem_size = _info.total / 1024 / 1024
-        
+
         # gpu, cpu list
         self.cpu_list = [[] for i in range(self.cpu_nums)]
         self.gpu_list = [[] for i in range(self.gpu_nums)]
         self.mem_list = []
 
         # 添加各个子图
-        self.fig = plt.figure(num=3, figsize=(20, 12),dpi=80)
-        self.cpu_subplot = self.fig.add_subplot(3,1,1)  
-        self.gpu_subplot = self.fig.add_subplot(3,1,2)
-        self.mem_subplot = self.fig.add_subplot(3,1,3)
+        self.fig = plt.figure(num=3, figsize=(20, 12), dpi=80)
+        self.cpu_subplot = self.fig.add_subplot(3, 1, 1)
+        self.gpu_subplot = self.fig.add_subplot(3, 1, 2)
+        self.mem_subplot = self.fig.add_subplot(3, 1, 3)
 
     def monitor(self):
         """
@@ -63,19 +62,20 @@ class CpuGpuMemoryInfo(object):
             self.sample_nums += 1
 
             # 获取cpu信息
-            cpu_used_rate_list = psutil.cpu_percent(interval=0.0, percpu=True)
+            cpu_used_rate_list = psutil.cpu_percent(interval=1, percpu=True)
             for cpu_ind, per_cpu_use_rate in enumerate(cpu_used_rate_list):
                 self.cpu_list[cpu_ind].append(per_cpu_use_rate)
             # 清除原图像
             self.cpu_subplot.cla()
-            #设置cpu坐标轴属性
+            # 设置cpu坐标轴属性
             self.cpu_subplot.set_title("CPU INFO")
             self.cpu_subplot.set_xlabel("Time")
             self.cpu_subplot.set_ylabel("Use Rate")
             self.cpu_subplot.grid()
             # 画出每个cpu的折线
             for ind, cpu in enumerate(self.cpu_list):
-                self.cpu_subplot.scatter(self.time_plot_list, cpu, label='cpu-' + str(ind))
+                self.cpu_subplot.scatter(
+                    self.time_plot_list, cpu, label='cpu-' + str(ind))
                 self.data_dict["cpu-" + str(ind)] = [self.time_plot_list, cpu]
             # 坐标自动调整
             self.cpu_subplot.autoscale()
@@ -95,7 +95,7 @@ class CpuGpuMemoryInfo(object):
             self.gpu_subplot.set_xlabel("Time")
             self.gpu_subplot.set_ylabel("Used(Mib)")
             self.gpu_subplot.grid()
-             # 画出每个gpu的折线
+            # 画出每个gpu的折线
             for ind, gpu in enumerate(self.gpu_list):
                 self.gpu_subplot.plot(gpu, label='gpu-' + str(ind))
                 self.data_dict['gpu-'+str(ind)] = [self.time_plot_list, gpu]
@@ -125,8 +125,8 @@ class CpuGpuMemoryInfo(object):
             # 输出采样次数
             if (self.sample_nums % 10 == 0 and self.sample_nums != 0):
                 plt.savefig(self.save_location)
-                print("save figure success, sample count = {}, now time = {}".\
-                     format(self.sample_nums, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+                print("save figure success, sample count = {}, now time = {}".
+                      format(self.sample_nums, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
                 # 保存数据
                 self.save_data(self.data_dict)
 
@@ -149,38 +149,40 @@ class CpuGpuMemoryInfo(object):
         sample_step = len(x_list) / n
 
         # 按照步长采样
-        x_list = [item for ind, item in enumerate(x_list) if ind % sample_step == 0 ]
-        y_list = [item for ind, item in enumerate(y_list) if ind % sample_step == 0 ]
+        x_list = [item for ind, item in enumerate(
+            x_list) if ind % sample_step == 0]
+        y_list = [item for ind, item in enumerate(
+            y_list) if ind % sample_step == 0]
         print(x_list)
         print(y_list)
 
-        return x_list, y_list 
+        return x_list, y_list
 
     def data_fitting(self, x_list, y_list):
         """
         对数据进行拟合
         """
         y = np.array(y_list)
-        
+
         # 用3次多项式拟合
         f1 = np.polyfit(x_list, y, 3)
         p1 = np.poly1d(f1)
         # print(p1)
-        
+
         # 也可使用yvals=np.polyval(f1, x)
-        yvals = p1(x_list)  #拟合y值
+        yvals = p1(x_list)  # 拟合y值
 
         return x_list, yvals
-
 
     def save_data(self, dict):
         """
         将数据保存到文件, 方便后续画图分析
         """
         file_name = "cpu-gpu-mem-info.csv"
-        pd.DataFrame.from_dict(dict, orient='index').T.to_csv(file_name, index=False)
-        print("save data to file: {} success, now time = {}".format(file_name, 
-                            time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+        pd.DataFrame.from_dict(dict, orient='index').T.to_csv(
+            file_name, index=False)
+        print("save data to file: {} success, now time = {}".format(file_name,
+                                                                    time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
 
     def load_data(self):
         """
@@ -191,19 +193,19 @@ class CpuGpuMemoryInfo(object):
         df = pd.DataFrame(file)
         data_dict = df.to_dict()
         return data_dict
-    
+
     def draw_cpu_image(self):
         """
         cpu图像
         """
         # 清除原图像
         self.cpu_subplot.cla()
-        #设置cpu坐标轴属性
+        # 设置cpu坐标轴属性
         self.cpu_subplot.set_title("CPU INFO")
         self.cpu_subplot.set_xlabel("Time")
         self.cpu_subplot.set_ylabel("Use Rate")
         self.cpu_subplot.grid()
-        
+
         data_dict = self.load_data()
 
         # 从数据中获取cpu个数
@@ -227,7 +229,8 @@ class CpuGpuMemoryInfo(object):
             x_list_new, y_list_new = self.data_fitting(x_list, y_list)
 
            #  self.cpu_subplot.plot(x_list, y_list, 's', label='original values')
-            self.cpu_subplot.plot(x_list_new, y_list_new, label='cpu-' + str(i))
+            self.cpu_subplot.plot(x_list_new, y_list_new,
+                                  label='cpu-' + str(i))
         print("total mean cpu = {}".format(cpu_mean / cpu_count))
         # 坐标自动调整
         self.cpu_subplot.set_xlim(0, x_list[len(x_list) - 1])
@@ -274,7 +277,6 @@ class CpuGpuMemoryInfo(object):
         print("gpu-0 min = {}".format(min(gpu_0)))
         print("finished draw gpu image")
 
-
         self.gpu_subplot.set_xlim(0, x_list[len(x_list) - 1])
         self.gpu_subplot.set_ylim(0, 12288)
         # 设置图例位置,loc可以为[upper, lower, left, right, center]
@@ -306,7 +308,6 @@ class CpuGpuMemoryInfo(object):
         self.mem_subplot.legend(loc='right', shadow=True)
         print("finished draw memory image")
 
-
     def save_image(self):
         """
         保存图像
@@ -337,10 +338,9 @@ class CpuGpuMemoryInfo(object):
 
 
 if __name__ == "__main__":
-    monitor = CpuGpuMemoryInfo("./test.png")
-    # monitor.run()
-    monitor.draw()
+    monitor = CpuGpuMemoryInfo("./merge-cluster.png")
+    monitor.run()
+    # monitor.draw()
     # data_dict = monitor.load_data()
     # print(ast.literal_eval(data_dict["mem"][1]))
     # print(len(ast.literal_eval(data_dict["mem"][1])))
-    
